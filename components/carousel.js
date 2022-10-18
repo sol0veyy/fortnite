1665ba7f4585081f39bd6bf288873bba272ce941
solovey <solovevdaniil03@mail.ru>
2022-10-18 20:39:25 +0300

    Update

---
+++ components/carousel.js
@@ -0,0 +1,30 @@
+let offset = 0; // смещение от левого края
+const buttons = document.querySelectorAll('button');
+const sliderLine = document.querySelector('.slider-line');
+const firstButton = document.querySelector('.first-slide');
+const secondButton = document.querySelector('.second-slide');
+const thirdButton = document.querySelector('.third-slide');
+
+document.querySelector('.first-slide').addEventListener('click', function(){
+    sliderLine.style.left = 100 + '%';
+    clearButton()
+    buttons[0].style.background = 'rgb(178, 234, 252)';
+});
+
+document.querySelector('.second-slide').addEventListener('click', function(){
+    sliderLine.style.left = 0;
+    clearButton()
+    buttons[1].style.background = 'rgb(178, 234, 252)';
+});
+
+document.querySelector('.third-slide').addEventListener('click', function(){
+    sliderLine.style.left = -100 + '%';
+    clearButton()
+    buttons[2].style.background = 'rgb(178, 234, 252)';
+});
+
+function clearButton() {
+    buttons.forEach(button => {
+        button.style.background = 'rgb(53, 71, 87, 0.7)';
+    });
+}
--- index.php
+++ index.php
@@ -1,20 +1,41 @@
+<?php
+    require('vendor/mysql.php');
+    $result = $mysql -> query("SELECT * FROM `news` ORDER BY id DESC LIMIT 3");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/news.css">
+    <link rel="stylesheet" href="assets/css/news.css"> 
+    <link rel="stylesheet" href="components/style.css">
     <title>Главная</title>
 </head>
 <body>
+    
     <?php require('vendor/header.php'); ?>
-        
-    <Carousel>
-        <template #slides>
-            <Slide v-for="slide in 10" :key="slide">
-            </Slide>
-        </template>
-    </Carousel>
+
+    <div class="container">
+        <div class="slider">
+            <div class="slider-line">
+                <?php
+                while ($news = mysqli_fetch_array($result)) {
+                    echo "
+                        <img src='{$news['picture']}'>
+                    ";
+                }  
+                ?>
+            </div>
+        </div>
+    </div>
+    <div class="box-button">
+        <button class="first-slide"></button>
+        <button class="second-slide"></button>
+        <button class="third-slide"></button>
+    </div>
+    
+    <script src="components/carousel.js"></script>
 </body>
 </html>
